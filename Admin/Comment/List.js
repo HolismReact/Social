55ea898331858b1a751ce3f34052263b47b7bf8c
Saeed Nemati <sn.1361@gmail.com>
2022-01-29 14:27:08 +0330

    committed by script

--- Admin/Comment/List.js
+++ Admin/Comment/List.js
@@ -1,5 +1,28 @@
+import { List, Text } from '@List'
+
+const filters = <>
+    <Text
+        column='Body'
+        placeholder='Body'
+    />
+</>
+
+const headers = <>
+    <th>Name</th>
+</>
+
+const row = (item) => <>
+    <td>{item.body}</td>
+</>
+
 const Comments = () => {
-    return <div>Comments</div>
+    return <List
+        title='Comments'
+        entityType='Comment'
+        filters={filters}
+        headers={headers}
+        row={row}
+    />
 }
 
 export default Comments
