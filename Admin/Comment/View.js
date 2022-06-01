87fe8d3a645164102d63c1a7041ffba3afe50fff
Saeed Nemati <sn.1361@gmail.com>
2022-06-01 09:59:09 +0430

    committed by script

--- Admin/Comment/View.js
+++ Admin/Comment/View.js
@@ -1,8 +1,13 @@
 import CommentIcon from '@mui/icons-material/Comment';
 import { ItemAction } from '@List'
 
-const ViewComments = ({ entityType, entityGuid }) => {
+const ViewComments = ({
+    entityType,
+    entityGuid,
+    ...rest
+}) => {
     return <ItemAction
+        {...rest}
         title='View comments'
         icon={CommentIcon}
         goTo={`/comments?entityType=${entityType}&entityGuid=${entityGuid}`}
